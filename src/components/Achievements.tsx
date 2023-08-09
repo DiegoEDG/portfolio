373e694f7d8fcb582e7cce530e416478d7ce2eb8
Digudev <1.5.dg.diego@gmail.com>
2023-08-09 12:08:40 -0600

    DATA: Mock data added

--- src/components/Achievements.tsx
+++ src/components/Achievements.tsx
@@ -1,3 +1,4 @@
+import { AchievementDB } from '@/data';
 import { CheckIcon } from '@/icons';
 import styles from './Achievements.module.css';
 
@@ -6,35 +7,14 @@ const Achievements = () => {
 		<section className={styles.achieveContainer}>
 			<h3 className={styles.title}>Achievements</h3>
 			<div className={styles.cardContainer}>
-				<article className={styles.card}>
-					<div>
-						<CheckIcon />
-					</div>
-					<p className={styles.description}>
-						Leading a system migration to modern technologies while implementing and adhering to best-in-class
-						development practices.
-					</p>
-				</article>
-				<article className={styles.card}>
-					<div>
-						<CheckIcon />
-					</div>
-					<p className={styles.description}>Providing mentorship to a compact team of developers within the company.</p>
-				</article>
-				<article className={styles.card}>
-					<div>
-						<CheckIcon />
-					</div>
-					<p className={styles.description}>
-						Increased sales, accompanied by positive user comments thanks to an impactful and strategic design.
-					</p>
-				</article>
-				<article className={styles.card}>
-					<div>
-						<CheckIcon />
-					</div>
-					<p className={styles.description}>Getting Amazon Web Services practitioner certificate</p>
-				</article>
+				{AchievementDB.map(({ description }) => (
+					<article className={styles.card}>
+						<div>
+							<CheckIcon />
+						</div>
+						<p className={styles.description}>{description}</p>
+					</article>
+				))}
 			</div>
 		</section>
 	);
---
+++ src/data/AchievementsDB.ts
@@ -0,0 +1,19 @@
+interface Achievement {
+	description: string;
+}
+
+export const AchievementDB: Achievement[] = [
+	{
+		description:
+			'Leading a system migration to modern technologies while implementing and adhering to best-in-class development practices.'
+	},
+	{
+		description: 'Providing mentorship to a compact team of developers within the company.'
+	},
+	{
+		description: 'Increased sales, accompanied by positive user comments thanks to an impactful and strategic design.'
+	},
+	{
+		description: 'Getting Amazon Web Services practitioner certificate'
+	}
+];
