7d12ca160fd8aec131cadaedb3966183621aff7e
Digudev <1.5.dg.diego@gmail.com>
2023-08-08 19:44:53 -0600

    CHORES: Barrel file added

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+export { default as Achievements } from './Achievements';
+export { default as Experience } from './Experience';
+export { default as Footer } from './Footer';
+export { default as Hero } from './Hero';
+export { default as Navbar } from './Navbar';
+export { default as ProjectCard } from './ProjectCard';
+export { default as Projects } from './Projects';
+export * from './TechStack';
+export { default as Technologies } from './Technologies';
