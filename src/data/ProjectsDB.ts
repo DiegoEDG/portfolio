eb636392a9173b971764633b062dd86662f2abd4
Digudev <1.5.dg.diego@gmail.com>
2023-08-10 14:21:15 -0600

    DATA: Links projects added

--- src/data/ProjectsDB.ts
+++ src/data/ProjectsDB.ts
@@ -19,42 +19,39 @@ export const ProjectsDB: IProject[] = [
 		name: 'Ense Studio Landing Page',
 		description: 'Amazing landing page to increase sales for a digital studio',
 		image: EnseImg,
-		ghLink: 'https://',
-		demoLink: 'https://'
+		ghLink: 'https://github.com/DiegoEDG/ense-landing',
+		demoLink: 'https://www.ensestudio.com/'
 	},
 	{
 		name: 'Movie App',
 		description: 'Mobile App to consult movies',
 		image: MoviesImg,
-		ghLink: 'https://',
-		demoLink: 'https://'
+		ghLink: 'https://github.com/DiegoEDG/MoviesApp'
 	},
 	{
 		name: 'Pokedex',
 		description: 'Mobile App doing fetching pokeApi for consult pokemon information',
 		image: PokedexImg,
-		ghLink: 'https://',
-		demoLink: 'https://'
+		ghLink: 'https://github.com/DiegoEDG/PokedexRN'
 	},
 	{
 		name: 'HooBank Landing Page',
 		description: 'Amazing landing page for a fake bank',
 		image: HoobankImg,
-		ghLink: 'https://',
-		demoLink: 'https://'
+		ghLink: 'https://github.com/DiegoEDG/Hoobank-landingPage',
+		demoLink: 'https://diegoedg.github.io/Hoobank-landingPage/'
 	},
 	{
 		name: 'Budget Manager',
 		description: 'Budger manager app for control money spend',
 		image: BudgetImg,
-		ghLink: 'https://',
-		demoLink: 'https://'
+		ghLink: 'https://github.com/DiegoEDG/control-de-gastos',
+		demoLink: 'https://romantic-kowalevski-b69217.netlify.app/'
 	},
 	{
 		name: 'CLI To-Do App ',
 		description: 'To-Do app for CLI',
 		image: CliImg,
-		ghLink: 'https://',
-		demoLink: 'https://'
+		ghLink: 'https://github.com/DiegoEDG/task-manager-console-app'
 	}
 ];
